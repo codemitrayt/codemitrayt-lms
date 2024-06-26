ba4b041e07f09895f7103d311ad26590910affc1
codemitrayt <codemitrayt@gmail.com>
2024-06-26 20:47:17 +0530

    feat: add single course page

---
+++ src/app/dashboard/(routes)/courses/(routes)/[courseId]/page.tsx
@@ -0,0 +1,61 @@
+import db from "@/lib/db"
+import { redirect } from "next/navigation"
+
+import IconBadge from "@/components/ui/icon-badge"
+import TitleForm from "../_components/title-form"
+import { LayoutDashboard } from "lucide-react"
+import DescriptionForm from "../_components/description-form"
+import ImageForm from "../_components/image-form"
+
+type SingleCoursePage = {
+  params: {
+    courseId: string
+  }
+}
+const SingleCoursePage = async ({ params }: SingleCoursePage) => {
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  })
+
+  if (!course) return redirect("/dashboard/courses")
+
+  const courseField = [
+    course.title,
+    course.description,
+    course.imgUrl,
+    course.categoryId,
+    course.price,
+  ]
+
+  const totalFields = courseField.length
+  const completedFields = courseField.filter(Boolean).length
+  const completionText = `${completedFields}/${totalFields}`
+
+  return (
+    <div className="p-6">
+      <div className="flex flex-col">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course Setup</h1>
+          <span className="text-sm text-muted-foreground">
+            Completed all fields {completionText}
+          </span>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 mt-16">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboard} />
+              <h1 className="text-xl">Customize your course</h1>
+            </div>
+            <TitleForm initialData={course} courseId={params.courseId} />
+            <DescriptionForm initialData={course} courseId={params.courseId} />
+            <ImageForm initialData={course} courseId={params.courseId} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SingleCoursePage

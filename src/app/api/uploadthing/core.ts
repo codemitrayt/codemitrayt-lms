1666d4b7172f43d06837ef4d301503e3e2aaccc5
codemitrayt <codemitrayt@gmail.com>
2024-06-26 20:46:46 +0530

    feat: add uploadthing core

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,25 @@
+import { auth } from "@clerk/nextjs/server"
+import { createUploadthing, type FileRouter } from "uploadthing/next"
+import { UploadThingError } from "uploadthing/server"
+
+const f = createUploadthing()
+
+const handleAuth = () => {
+  const { userId } = auth()
+  if (!userId) new Error("Unathorized")
+  return { userId }
+}
+
+export const ourFileRouter = {
+  courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  courseAttachments: f(["text", "audio", "video", "audio", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  chapterVideo: f({ video: { maxFileSize: "512GB", maxFileCount: 1 } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+} satisfies FileRouter
+
+export type OurFileRouter = typeof ourFileRouter

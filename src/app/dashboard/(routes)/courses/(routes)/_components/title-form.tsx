9a3777ff9e805ec64b2b827c7b2a6ed7317f2d94
codemitrayt <codemitrayt@gmail.com>
2024-06-26 20:48:04 +0530

    feat: add title-form cmponent

---
+++ src/app/dashboard/(routes)/courses/(routes)/_components/title-form.tsx
@@ -0,0 +1,120 @@
+"use client"
+
+import * as z from "zod"
+import axios from "axios"
+import { useState } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { useRouter } from "next/navigation"
+import toast from "react-hot-toast"
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+import { Pencil, X } from "lucide-react"
+
+interface TitleFormProps {
+  initialData: {
+    title: string
+  }
+  courseId: string
+}
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: "Title is required." }),
+})
+
+type FormSchemaType = z.infer<typeof formSchema>
+
+const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: initialData.title,
+    },
+  })
+
+  const router = useRouter()
+  const { isSubmitting, isValid } = form.formState
+  const [isEdit, setIsEdit] = useState(false)
+  const toggleEditButton = () => setIsEdit((cur) => !cur)
+
+  const onSubmit = async (values: FormSchemaType) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values)
+      toast.success("Course updated successfully.")
+      toggleEditButton()
+      router.refresh()
+    } catch (error) {
+      toast.error("Something went wrong.")
+    }
+  }
+
+  return (
+    <div className="mt-6 border shadow-sm shadow-gray-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        <span>Course Title</span>
+        <Button
+          className="flex items-center gap-x-2"
+          variant="ghost"
+          onClick={toggleEditButton}
+        >
+          {isEdit ? (
+            <>
+              <X className="size-3" />
+              <span className="text-xs">Cancel</span>
+            </>
+          ) : (
+            <>
+              <Pencil className="size-3" />
+              <span className="text-xs">Edit Title</span>
+            </>
+          )}
+        </Button>
+      </div>
+      <div className="">
+        {isEdit ? (
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-4 mt-4"
+            >
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        disabled={isSubmitting}
+                        placeholder="e.g. 'Data Structure and Algorithms'"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex items-center justify-end">
+                <Button type="submit" disabled={!isValid || isSubmitting}>
+                  Save
+                </Button>
+              </div>
+            </form>
+          </Form>
+        ) : (
+          <p className="text-sm mt-2">{initialData.title}</p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default TitleForm

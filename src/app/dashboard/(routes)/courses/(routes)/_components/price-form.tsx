9180553aa7506803d6197965071e5db079adfb93
codemitrayt <codemitrayt@gmail.com>
2024-06-27 12:50:26 +0530

    build: add price-form component

---
+++ src/app/dashboard/(routes)/courses/(routes)/_components/price-form.tsx
@@ -0,0 +1,125 @@
+"use client"
+
+import * as z from "zod"
+import axios from "axios"
+import { useState } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { useRouter } from "next/navigation"
+import toast from "react-hot-toast"
+import { cn } from "@/lib/utils"
+import { formatPrice } from "@/lib/format"
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+import { Pencil, X } from "lucide-react"
+
+interface PriceFormProps {
+  initialData: {
+    price: number | null
+  }
+  courseId: string
+}
+
+const formSchema = z.object({
+  price: z.coerce.number(),
+})
+
+type FormSchemaType = z.infer<typeof formSchema>
+
+const PriceForm = ({ initialData, courseId }: PriceFormProps) => {
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      price: initialData.price ? initialData.price : undefined,
+    },
+  })
+
+  const router = useRouter()
+  const { isSubmitting, isValid } = form.formState
+  const [isEdit, setIsEdit] = useState(false)
+  const toggleEditButton = () => setIsEdit((cur) => !cur)
+
+  const onSubmit = async (values: FormSchemaType) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values)
+      toast.success("Course updated successfully.")
+      toggleEditButton()
+      router.refresh()
+    } catch (error) {
+      toast.error("Something went wrong.")
+    }
+  }
+
+  return (
+    <div className="mt-6 border shadow-sm shadow-gray-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        <span>Course Price</span>
+        <Button
+          className="flex items-center gap-x-2"
+          variant="ghost"
+          onClick={toggleEditButton}
+        >
+          {isEdit ? (
+            <>
+              <X className="size-3" />
+              <span className="text-xs">Cancel</span>
+            </>
+          ) : (
+            <>
+              <Pencil className="size-3" />
+              <span className="text-xs">Edit price</span>
+            </>
+          )}
+        </Button>
+      </div>
+      <div className="">
+        {isEdit ? (
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-4 mt-4"
+            >
+              <FormField
+                control={form.control}
+                name="price"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        disabled={isSubmitting}
+                        placeholder="Set price for your course"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex items-center justify-end">
+                <Button type="submit" disabled={!isValid || isSubmitting}>
+                  Save
+                </Button>
+              </div>
+            </form>
+          </Form>
+        ) : (
+          <p className={cn("text-sm mt-2", !initialData.price && "italic")}>
+            {initialData?.price ? formatPrice(initialData.price) : "No price"}
+          </p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default PriceForm

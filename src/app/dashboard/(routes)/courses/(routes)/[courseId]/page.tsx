5bb5bbe060c47feeb798bd23c0723e3bf946d1b7
codemitrayt <codemitrayt@gmail.com>
2024-06-27 12:50:13 +0530

    feat: add price-form

--- src/app/dashboard/(routes)/courses/(routes)/[courseId]/page.tsx
+++ src/app/dashboard/(routes)/courses/(routes)/[courseId]/page.tsx
@@ -3,10 +3,11 @@ import { redirect } from "next/navigation"
 
 import IconBadge from "@/components/ui/icon-badge"
 import TitleForm from "../_components/title-form"
-import { LayoutDashboard } from "lucide-react"
+import { IndianRupee, LayoutDashboard, ListChecks } from "lucide-react"
 import DescriptionForm from "../_components/description-form"
 import ImageForm from "../_components/image-form"
 import CategoryForm from "../_components/category-form"
+import PriceForm from "../_components/price-form"
 
 type SingleCoursePage = {
   params: {
@@ -58,11 +59,12 @@ const SingleCoursePage = async ({ params }: SingleCoursePage) => {
             Completed all fields {completionText}
           </span>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 mt-16">
+
+        <div className="grid grid-cols-1 md:grid-cols-2 mt-16 gap-6">
           <div>
             <div className="flex items-center gap-x-2">
               <IconBadge icon={LayoutDashboard} />
-              <h1 className="text-xl">Customize your course</h1>
+              <h1 className="text-lg font-medium">Customize your course</h1>
             </div>
             <TitleForm initialData={course} courseId={params.courseId} />
             <DescriptionForm initialData={course} courseId={params.courseId} />
@@ -72,6 +74,24 @@ const SingleCoursePage = async ({ params }: SingleCoursePage) => {
               courseId={params.courseId}
               options={categories}
             />
+          </div>
+          <div className="flex flex-col gap-6">
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={ListChecks} />
+                <h1 className="text-lg font-medium">Course chapters</h1>
+              </div>
+              <div>TODO: Chapters</div>
+            </div>
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={IndianRupee} />
+                <h1 className="text-lg font-medium">Sell your course</h1>
+              </div>
+              <div>
+                <PriceForm initialData={course} courseId={params.courseId} />
+              </div>
+            </div>
           </div>
         </div>
       </div>

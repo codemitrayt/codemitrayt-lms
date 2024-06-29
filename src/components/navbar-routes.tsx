81292701af1ccfbdc7ac0aaeef03d19bff2fe281
codemitrayt <codemitrayt@gmail.com>
2024-06-29 17:18:47 +0530

    feat: add mode toggle

--- src/components/navbar-routes.tsx
+++ src/components/navbar-routes.tsx
@@ -1,10 +1,12 @@
 import { UserButton } from "@clerk/nextjs"
+import ModeToggle from "./mode-toggle"
 
 const NavbarRoutes = () => {
   return (
     <div className="flex items-center justify-between w-full">
       <div></div>
-      <div className="flex items-center space-x-2 justify-center">
+      <div className="flex items-center space-x-4 justify-center">
+        <ModeToggle />
         <UserButton />
       </div>
     </div>

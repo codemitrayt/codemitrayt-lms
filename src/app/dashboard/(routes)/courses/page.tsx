36426b8b0732bcfaa7d9f5615b4cca97644b4855
codemitrayt <codemitrayt@gmail.com>
2024-07-03 10:18:43 +0530

    refactor: course page component

--- src/app/dashboard/(routes)/courses/page.tsx
+++ src/app/dashboard/(routes)/courses/page.tsx
@@ -1,12 +1,23 @@
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { columns } from "./_components/columns"
+import { DataTable } from "./_components/data-table"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import db from "@/lib/db"
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const { userId } = auth()
+  if (!userId) return redirect("/")
+
+  const courses = await db.course.findMany({
+    include: { category: true },
+    orderBy: { createdAt: "desc" },
+  })
+
   return (
-    <div>
-      <Link href="/dashboard/courses/create">
-        <Button>Create Course</Button>
-      </Link>
+    <div className="relative">
+      <div className="container mx-auto">
+        <DataTable columns={columns} data={courses} />
+      </div>
     </div>
   )
 }

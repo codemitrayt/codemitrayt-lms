13cf91e91f724b64134d3541f283e3fe4e633368
codemitrayt <codemitrayt@gmail.com>
2024-07-06 12:40:40 +0530

    feat: seach input feature add in navbar

--- src/components/navbar-routes.tsx
+++ src/components/navbar-routes.tsx
@@ -1,10 +1,20 @@
+"use client"
+
 import { UserButton } from "@clerk/nextjs"
+import { usePathname } from "next/navigation"
+
+import SearchInput from "./search-input"
 import ModeToggle from "./mode-toggle"
 
 const NavbarRoutes = () => {
+  const pathname = usePathname()
+  const isSearchPage = pathname.includes("/search")
+
   return (
     <div className="flex items-center justify-between w-full">
-      <div></div>
+      <div className="relative">
+        <div className="hidden md:block">{isSearchPage && <SearchInput />}</div>
+      </div>
       <div className="flex items-center space-x-4 justify-center">
         <ModeToggle />
         <UserButton />

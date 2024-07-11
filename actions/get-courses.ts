9569dbc14b82a58f84550beabe751b4ff6346989
codemitrayt <codemitrayt@gmail.com>
2024-07-11 13:32:21 +0530

    feat: add get course action

---
+++ actions/get-courses.ts
@@ -0,0 +1,70 @@
+import { Course, Category } from "@prisma/client"
+
+import db from "@/lib/db"
+import { getProgress } from "./get-progress"
+
+type CourseWithProgressWithCategory = Course & {
+  category: Category | null
+  chapters: { id: string }[]
+  progress: number | null
+}
+
+type GetCourses = {
+  userId: string
+  title?: string
+  categoryId?: string
+}
+
+export const getCourses = async ({
+  userId,
+  title,
+  categoryId,
+}: GetCourses): Promise<CourseWithProgressWithCategory[]> => {
+  try {
+    const courses = await db.course.findMany({
+      where: {
+        isPublished: true,
+        title: {
+          contains: title,
+        },
+        categoryId,
+      },
+      include: {
+        category: true,
+        chapters: {
+          where: {
+            isPublished: true,
+          },
+          select: {
+            id: true,
+          },
+        },
+        purchases: {
+          where: {
+            userId,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    })
+
+    const courseWithProgressWithCategory: CourseWithProgressWithCategory[] =
+      await Promise.all(
+        courses.map(async (course) => {
+          if (course.purchases.length === 0)
+            return { ...course, progress: null }
+
+          const progressPercentage = await getProgress(userId, course.id)
+
+          return { ...course, progress: progressPercentage }
+        })
+      )
+
+    return courseWithProgressWithCategory
+  } catch (error) {
+    console.log("[GET_CODES] Error :: ", error)
+    return []
+  }
+}

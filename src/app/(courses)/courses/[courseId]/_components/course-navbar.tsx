89bd5baae3256b187dfcff7ecedbc5d06d81e2c3
codemitrayt <codemitrayt@gmail.com>
2024-07-11 13:34:05 +0530

    build(comp): course navbar component

---
+++ src/app/(courses)/courses/[courseId]/_components/course-navbar.tsx
@@ -0,0 +1,21 @@
+import NavbarRoutes from "@/components/navbar-routes"
+import { Chapter, Course, UserProgress } from "@prisma/client"
+import CourseMobileSidebar from "./course-mobile-sidebar"
+
+interface CourseNavbarProps {
+  course: Course & {
+    chapters: (Chapter & { userProgress: UserProgress[] | null })[]
+  }
+  progress: number
+}
+
+const CourseNavbar = ({ course, progress }: CourseNavbarProps) => {
+  return (
+    <div className="p-4 border-b h-full flex items-center shadow-sm">
+      <CourseMobileSidebar course={course} progress={progress} />
+      <NavbarRoutes />
+    </div>
+  )
+}
+
+export default CourseNavbar

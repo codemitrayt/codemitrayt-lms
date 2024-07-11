c7c1f568d555017a3cbc805b42fd88702e0fb810
codemitrayt <codemitrayt@gmail.com>
2024-07-11 13:35:48 +0530

    feat: add search functionality to the search courses

--- src/app/dashboard/(routes)/search/page.tsx
+++ src/app/dashboard/(routes)/search/page.tsx
@@ -1,11 +1,32 @@
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import { getCourses } from "actions/get-courses"
+
 import db from "@/lib/db"
 import Categories from "./_components/categories"
+import CoursesList from "./_components/courses-list"
 
-const SearchPage = async () => {
+interface SearchProps {
+  searchParams: {
+    categoryId: string
+    title: string
+  }
+}
+
+const SearchPage = async ({ searchParams }: SearchProps) => {
+  const { userId } = auth()
+  if (!userId) return redirect("/")
   const categories = await db.category.findMany({ orderBy: { name: "asc" } })
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  })
   return (
     <div className="p-6">
       <Categories categories={categories} />
+      <div className="mt-4">
+        <CoursesList items={courses} />
+      </div>
     </div>
   )
 }
